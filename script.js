5c5f098aca8b52b92c9e341300af136ffa7d43d8
Naman <namansaini1371@gmail.com>
2026-03-26 11:14:40 +0530

    added styles to movie cards

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const api="https://api.themoviedb.org/3/movie/popular?api_key=950726300c0085cae525f81415141462"
+const api="https://api.themoviedb.org/3/movie/upcoming?api_key=950726300c0085cae525f81415141462&page=1"
+//Variables
+let page=1
 //functions
 async function getData(api){
     const response=await fetch(api)
@@ -7,9 +9,14 @@ async function getData(api){
     data.results.forEach((movie)=>{
         const poster = "https://image.tmdb.org/t/p/w200" + movie.poster_path;
         const movieCard=document.createElement("div")
+        movieCard.className="movieCard"
         movieCard.innerHTML=`
-        <img src="${poster}"/>`
+        <img src="${poster}" class="movieImage"/>
+        <h3 class="movieTitle">${movie.title}</h3>
+        <p class="movieRating">Rating⭐: ${movie.vote_average}</p>`
         movieDiv.appendChild(movieCard)
     })
 }
+//Event Listners
+
 getData(api)
