373e097ef7614f4a074ef48d28b544bb23dbed3b
Naman <namansaini1371@gmail.com>
2026-03-27 22:25:57 +0530

    Added card styles

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ async function getData(api){
         movieCard.className="movieCard"
         movieCard.innerHTML=`
         <img src="${poster}" class="movieImage" alt="${movie.title}"/>
+        <div class="movieDetails">
         <a class="movieTitle" href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">${movie.title}</a>
-        <p class="movieRating">Rating⭐: ${movie.vote_average}</p>`
+        <p class="movieRating">Rating⭐: ${movie.vote_average}</p>
+        </div>`
         movieDiv.appendChild(movieCard)
     })
 }
